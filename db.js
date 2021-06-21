33be9ea8b5eacf887a97ea1d2fc5b7e5b8b9eb54
zile028 <zile028@gmail.com>
2021-06-22 00:44:26 +0200

    first commit

---
+++ db.js
@@ -0,0 +1,250 @@
+let movie = [
+  {
+    year: 2013,
+    title: "Rush",
+    info: {
+      directors: ["Ron Howard"],
+      release_date: "2013-09-02T00:00:00Z",
+      rating: 8.3,
+      genres: ["Action", "Biography", "Drama", "Sport"],
+      image_url:
+        "https://m.media-amazon.com/images/M/MV5BOWEwODJmZDItYTNmZC00OGM4LThlNDktOTQzZjIzMGQxODA4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+      plot:
+        "A re-creation of the merciless 1970s rivalry between Formula One rivals James Hunt and Niki Lauda.",
+      rank: 2,
+      running_time_secs: 7380,
+      actors: ["Daniel Bruhl", "Chris Hemsworth", "Olivia Wilde"],
+    },
+  },
+
+  {
+    year: 2013,
+    title: "This Is the End",
+    info: {
+      directors: ["Evan Goldberg", "Seth Rogen"],
+      release_date: "2013-06-03T00:00:00Z",
+      rating: 7.2,
+      genres: ["Comedy", "Fantasy"],
+      image_url:
+        "https://occ-0-300-299.1.nflxso.net/dnm/api/v6/XsrytRUxks8BtTRf9HNlZkW2tvY/AAAABR5yMp5rCC3AXTH_vkizEn9n8PVck3KPEfypEFyWCqvh0O9oucREImp5Eh3pSJh0u7_ojY9LDoeS8HayoDyO8np5c_MUIYjL7A.jpg",
+      plot:
+        "While attending a party at James Franco's house, Seth Rogen, Jay Baruchel and many other celebrities are faced with the apocalypse.",
+      rank: 6,
+      running_time_secs: 6420,
+      actors: ["James Franco", "Jonah Hill", "Seth Rogen"],
+    },
+  },
+  {
+    year: 2014,
+    title: "X-Men: Days of Future Past",
+    info: {
+      directors: ["Bryan Singer"],
+      release_date: "2014-05-21T00:00:00Z",
+      genres: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMTQ0NzIwNTA1MV5BMl5BanBnXkFtZTgwNjY2OTcwMDE@._V1_SX400_.jpg",
+      plot:
+        "The X-Men send Wolverine to the past to change a major historical event that could globally impact man and mutant kind.",
+      rank: 9,
+      actors: ["Jennifer Lawrence", "Hugh Jackman", "Michael Fassbender"],
+    },
+  },
+  {
+    year: 2014,
+    title: "Transformers: Age of Extinction",
+    info: {
+      directors: ["Michael Bay"],
+      release_date: "2014-06-25T00:00:00Z",
+      genres: ["Action", "Adventure", "Sci-Fi"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMTQyMDA5Nzg0Nl5BMl5BanBnXkFtZTgwNzA4NDcxMDE@._V1_SX400_.jpg",
+      plot:
+        "A mechanic and his daughter make a discovery that brings down Autobots and Decepticons - and a paranoid government official - on them.",
+      rank: 10,
+      actors: ["Mark Wahlberg", "Nicola Peltz", "Jack Reynor"],
+    },
+  },
+  {
+    year: 2013,
+    title: "Now You See Me",
+    info: {
+      directors: ["Louis Leterrier"],
+      release_date: "2013-05-21T00:00:00Z",
+      rating: 7.3,
+      genres: ["Crime", "Mystery", "Thriller"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMTY0NDY3MDMxN15BMl5BanBnXkFtZTcwOTM5NzMzOQ@@._V1_SX400_.jpg",
+      plot:
+        "An FBI agent and an Interpol detective track a team of illusionists who pull off bank heists during their performances and reward their audiences with the money.",
+      rank: 11,
+      running_time_secs: 6900,
+      actors: ["Jesse Eisenberg", "Common", "Mark Ruffalo"],
+    },
+  },
+  {
+    year: 2013,
+    title: "Gravity",
+    info: {
+      directors: ["Alfonso Cuaron"],
+      release_date: "2013-08-28T00:00:00Z",
+      rating: 8.2,
+      genres: ["Drama", "Sci-Fi", "Thriller"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BNjE5MzYwMzYxMF5BMl5BanBnXkFtZTcwOTk4MTk0OQ@@._V1_SX400_.jpg",
+      plot:
+        "A medical engineer and an astronaut work together to survive after an accident leaves them adrift in space.",
+      rank: 12,
+      running_time_secs: 5400,
+      actors: ["Sandra Bullock", "George Clooney", "Ed Harris"],
+    },
+  },
+  {
+    year: 2013,
+    title: "We're the Millers",
+    info: {
+      directors: ["Rawson Marshall Thurber"],
+      release_date: "2013-08-03T00:00:00Z",
+      rating: 7.2,
+      genres: ["Comedy", "Crime"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjA5Njc0NDUxNV5BMl5BanBnXkFtZTcwMjYzNzU1OQ@@._V1_SX400_.jpg",
+      plot:
+        "A veteran pot dealer creates a fake family as part of his plan to move a huge shipment of weed into the U.S. from Mexico.",
+      rank: 13,
+      running_time_secs: 6600,
+      actors: ["Jason Sudeikis", "Jennifer Aniston", "Emma Roberts"],
+    },
+  },
+  {
+    year: 2013,
+    title: "Riddick",
+    info: {
+      directors: ["David Twohy"],
+      release_date: "2013-09-04T00:00:00Z",
+      rating: 6.8,
+      genres: ["Action", "Sci-Fi", "Thriller"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMTk5NzYwMzQ4MV5BMl5BanBnXkFtZTcwMjE5MTI1OQ@@._V1_SX400_.jpg",
+      plot:
+        "Left for dead on a sun-scorched planet, Riddick finds himself up against an alien race of predators. Activating an emergency beacon alerts two ships: one carrying a new breed of mercenary, the other captained by a man from Riddick's past.",
+      rank: 14,
+      running_time_secs: 7140,
+      actors: ["Vin Diesel", "Karl Urban", "Katee Sackhoff"],
+    },
+  },
+  {
+    year: 2013,
+    title: "The Family",
+    info: {
+      directors: ["Luc Besson"],
+      release_date: "2013-09-10T00:00:00Z",
+      rating: 6.5,
+      genres: ["Action", "Comedy", "Crime"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjE2MzI0MzkyNV5BMl5BanBnXkFtZTcwMjQ2MDM2OQ@@._V1_SX400_.jpg",
+      plot:
+        "The Manzoni family, a notorious mafia clan, is relocated to Normandy, France under the witness protection program, where fitting in soon becomes challenging as their old habits die hard.",
+      rank: 15,
+      running_time_secs: 6660,
+      actors: ["Robert De Niro", "Michelle Pfeiffer", "Dianna Agron"],
+    },
+  },
+  {
+    year: 2013,
+    title: "Star Trek Into Darkness",
+    info: {
+      directors: ["J.J. Abrams"],
+      release_date: "2013-05-02T00:00:00Z",
+      rating: 7.9,
+      genres: ["Action", "Adventure", "Sci-Fi"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMTk2NzczOTgxNF5BMl5BanBnXkFtZTcwODQ5ODczOQ@@._V1_SX400_.jpg",
+      plot:
+        "After the crew of the Enterprise find an unstoppable force of terror from within their own organization, Captain Kirk leads a manhunt to a war-zone world to capture a one man weapon of mass destruction.",
+      rank: 16,
+      running_time_secs: 7920,
+      actors: ["Chris Pine", "Zachary Quinto", "Zoe Saldana"],
+    },
+  },
+  {
+    year: 2014,
+    title: "Divergent",
+    info: {
+      directors: ["Neil Burger"],
+      release_date: "2014-03-20T00:00:00Z",
+      genres: ["Action", "Adventure", "Romance", "Sci-Fi"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjIyOTEyODQ4OV5BMl5BanBnXkFtZTgwMDIwNzEzMDE@._V1_SX400_.jpg",
+      plot:
+        "Beatrice Prior, a teenager with a special mind, finds her life threatened when an authoritarian leader seeks to exterminate her kind in her effort to seize control of their divided society.",
+      rank: 20,
+      actors: ["Shailene Woodley", "Kate Winslet", "Zoe Kravitz"],
+    },
+  },
+  {
+    year: 2013,
+    title: "We Are What We Are",
+    info: {
+      directors: ["Jim Mickle"],
+      release_date: "2013-01-18T00:00:00Z",
+      rating: 6.2,
+      genres: ["Drama", "Horror", "Thriller"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjI3NjI3NjAyN15BMl5BanBnXkFtZTgwODE3NzMxMDE@._V1_SX400_.jpg",
+      plot:
+        "The Parkers, a reclusive family who follow ancient customs, find their secret existence threatened as a torrential downpour moves into their area, forcing daughters Iris and Rose to assume responsibilities beyond those of a typical family.",
+      rank: 21,
+      running_time_secs: 6300,
+      actors: ["Bill Sage", "Ambyr Childers", "Julia Garner"],
+    },
+  },
+  {
+    year: 2013,
+    title: "Iron Man 3",
+    info: {
+      directors: ["Shane Black"],
+      release_date: "2013-04-18T00:00:00Z",
+      rating: 7.4,
+      genres: ["Action", "Adventure", "Sci-Fi"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjIzMzAzMjQyM15BMl5BanBnXkFtZTcwNzM2NjcyOQ@@._V1_SX400_.jpg",
+      plot:
+        "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.",
+      rank: 22,
+      running_time_secs: 7800,
+      actors: ["Robert Downey Jr.", "Guy Pearce", "Gwyneth Paltrow"],
+    },
+  },
+  {
+    year: 2014,
+    title: "The Amazing Spider-Man 2",
+    info: {
+      directors: ["Marc Webb"],
+      release_date: "2014-04-17T00:00:00Z",
+      genres: ["Action", "Adventure", "Fantasy"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjE0MDAwNTUzNF5BMl5BanBnXkFtZTcwNDQ4NDYxOQ@@._V1_SX400_.jpg",
+      plot:
+        "Spider-Man squares off against the Rhino and the powerful Electro while struggling to keep his promise to leave Gwen Stacey out of his dangerous life.",
+      rank: 23,
+      actors: ["Andrew Garfield", "Emma Stone", "Jamie Foxx"],
+    },
+  },
+  {
+    year: 2013,
+    title: "Curse of Chucky",
+    info: {
+      directors: ["Don Mancini"],
+      release_date: "2013-08-02T00:00:00Z",
+      rating: 5.7,
+      genres: ["Horror", "Thriller"],
+      image_url:
+        "http://ia.media-imdb.com/images/M/MV5BMjkxMjM0MDE3OF5BMl5BanBnXkFtZTcwMDQ3NzI4OQ@@._V1_SX400_.jpg",
+      plot:
+        "After her mother's mysterious death, Nica begins to suspect that the talking, red-haired doll her visiting niece has been playing with may be the key to recent bloodshed and chaos.",
+      rank: 24,
+      running_time_secs: 5820,
+      actors: ["Chantal Quesnelle", "Fiona Dourif", "Danielle Bisutti"],
+    },
+  },
+];
